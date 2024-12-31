a7278abe4ced0f22fc8eb8071a91a00d98791d94
RayNguyen23 <rayjohnson4126@gmail.com>
2024-12-31 16:24:42 +0700

    Updated

--- apis/api.ts
+++ apis/api.ts
@@ -1,2 +1,2 @@
-// export const apiUrl = "https://7bc5-42-118-49-194.ngrok-free.app";
-export const apiUrl = "http://localhost:3302";
+export const apiUrl = "https://7bc5-42-118-49-194.ngrok-free.app";
+// export const apiUrl = "http://localhost:3302";
